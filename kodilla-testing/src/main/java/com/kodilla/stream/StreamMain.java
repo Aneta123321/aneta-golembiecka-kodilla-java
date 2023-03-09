f2444190123e86ae2f5385ee47dd68e1023a8d76
Aneta G <aneta.golembiecka@gmail.com>
2023-03-09 16:45:17 +0100

    7.2. niemutowalny użytkownik forum

--- kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.immutable.ForumUser;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -8,5 +9,6 @@ public class StreamMain {
         poemBeautifier.beautify("Za oknem pada snieg!", text -> text.toLowerCase());
         poemBeautifier.beautify("Za oknem pada snieg!", text -> text.toUpperCase());
         poemBeautifier.beautify("Za oknem pada snieg!", text -> "*** ".concat(text).concat(" ***"));
+
     }
 }
