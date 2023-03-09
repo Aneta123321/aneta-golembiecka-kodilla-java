568ce415de798cecd7cea2af392592dcd81f4804
Aneta G <aneta.golembiecka@gmail.com>
2023-03-09 16:11:54 +0100

    7.1. upiększacz tekstów

---
+++ kodilla-testing/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,12 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Za oknem pada snieg!", text -> text.toLowerCase());
+        poemBeautifier.beautify("Za oknem pada snieg!", text -> text.toUpperCase());
+        poemBeautifier.beautify("Za oknem pada snieg!", text -> "*** ".concat(text).concat(" ***"));
+    }
+}
