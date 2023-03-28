6b940717125a516359e3bb32b18499733365f7dd
Aneta G <aneta.golembiecka@gmail.com>
2023-03-28 16:55:51 +0200

    7.8.Papier kamien nozyce

---
+++ Kodilla-rps/src/main/java/com/kodilla/Move.java
@@ -0,0 +1,18 @@
+package com.kodilla;
+
+public enum Move {
+    ROCK("kamien"),
+    PAPER("papier"),
+    SCISSORS("nozyczki");
+
+    private String value;
+
+    Move(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
---
+++ Kodilla-rps/src/main/java/com/kodilla/RpsRunner.java
@@ -0,0 +1,9 @@
+package com.kodilla;
+
+public class RpsRunner {
+
+    public static void main(String[] args) {
+        RpsGame rpsGame = new RpsGame();
+        rpsGame.initGame();
+    }
+    }
