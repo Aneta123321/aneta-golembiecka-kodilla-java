3699c26c6e009f754ef9c3a29428892cfe1bdd07
Aneta G <aneta.golembiecka@gmail.com>
2023-03-28 23:38:53 +0200

    7.5 streamggg

--- kodilla-testing/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-testing/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -5,6 +5,7 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
     static double getAverage(int[] numbers){
 
+
         IntStream.range(0, numbers.length)
                 .map(n->numbers[n])
                 .forEach(System.out::println);
